ee233dff264676501ba3b0bd78221b76eacea0e9
Duy Nhan <duynhannguyenn@gmail.com>
2024-04-22 03:53:29 +0700

    Initial project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import MainSection from './components/mainSection/MainSection';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainSection />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
