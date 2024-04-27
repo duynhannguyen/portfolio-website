de721e24c2e3a5cccf0844823aac0db703c7925c
duynhannguyen <130212806+duynhannguyen@users.noreply.github.com>
2024-04-28 05:11:06 +0700

    feat: write about me section

--- src/components/contentSection/ContentSection.tsx
+++ src/components/contentSection/ContentSection.tsx
@@ -1,6 +1,45 @@
 import "./ContentSection.css";
 const ContentSection = () => {
-  return <div className="content-section-wrap">ContentSection</div>;
+  // const myLife = {
+  //   name: "Nguyen Duy Nhan",
+  //   yearOfBirth: 1999,
+  //   address: 'Ho Chi Minh City',
+  //   interest: ['learning','coding','reading'],
+  //   work: {
+  //     jobTitle: 'Web & SoftWare developer',
+  //     curentLevel:'INITAL_LEVEL',
+  //     getBetter: function(step){
+  //       const targetLevel = this.curentLevel + step
+  //       while(this.curentLevel < targetLevel){
+  //         this.curentLevel ++;
+  //         toWork()
+  //       }
+  //       toWork()
+  //       this.getBetter(step)
+  //     }
+  //   }
+
+  // }
+  // myLife.work.getBetter(STEP)
+  return (
+    <div className="content-section-wrap">
+      <div className="introdution">
+        /* In terms of skills, I currently work with JavaScript, React, and
+        related tools for front-end development. I'm also actively learning to
+        become a full-stack developer in the near future. My approach is
+        proactive, focusing on delivering high-quality, reusable code. I take
+        initiative in self-learning and keeping up with new technologies. When I
+        encounter challenges, I make an effort to find solutions independently
+        before seeking help. My goal is to collaborate with a team to create
+        tech products, enhance my skills, and contribute to our organization's
+        success in meeting market demands. /*
+      </div>
+
+      <div className="my-life-obj">
+        <img src="/myLife_dark.png" alt="myLife" />
+      </div>
+    </div>
+  );
 };
 
 export default ContentSection;
--- src/components/folderStructure/FolderStructure.tsx
+++ src/components/folderStructure/FolderStructure.tsx
@@ -1,100 +1,86 @@
-import './FolderStructure.css';
-import { DownOutlined } from '@ant-design/icons';
-import { Tree } from 'antd';
-import type { TreeDataNode, TreeProps } from 'antd';
+import "./FolderStructure.css";
+import { DownOutlined } from "@ant-design/icons";
+import { Tree } from "antd";
+import type { TreeDataNode, TreeProps } from "antd";
 
 const treeData: TreeDataNode[] = [
   {
-    title: 'Bio',
-    key: '0-0',
+    title: "Bio",
+    key: "0-0",
     children: [
       {
-        title: 'parent 1-0',
-        key: '0-0-0',
+        title: "About me",
+        key: "0-0-0",
+      },
+      {
+        title: "parent 1-1",
+        key: "0-0-1",
         children: [
           {
-            title: 'leaf',
-            key: '0-0-0-0',
-          },
-          {
-            title: 'leaf',
-            key: '0-0-0-1',
-          },
-          {
-            title: 'leaf',
-            key: '0-0-0-2',
+            title: "leaf",
+            key: "0-0-1-0",
           },
         ],
       },
       {
-        title: 'parent 1-1',
-        key: '0-0-1',
+        title: "parent 1-2",
+        key: "0-0-2",
         children: [
           {
-            title: 'leaf',
-            key: '0-0-1-0',
-          },
-        ],
-      },
-      {
-        title: 'parent 1-2',
-        key: '0-0-2',
-        children: [
-          {
-            title: 'leaf',
-            key: '0-0-2-0',
+            title: "leaf",
+            key: "0-0-2-0",
           },
           {
-            title: 'leaf',
-            key: '0-0-2-1',
+            title: "leaf",
+            key: "0-0-2-1",
           },
         ],
       },
     ],
   },
   {
-    title: 'Interests',
-    key: '1-0',
+    title: "Interests",
+    key: "1-0",
     children: [
       {
-        title: 'parent 1-0',
-        key: '1-0-0',
+        title: "parent 1-0",
+        key: "1-0-0",
         children: [
           {
-            title: 'leaf',
-            key: '1-0-0-1',
+            title: "leaf",
+            key: "1-0-0-1",
           },
           {
-            title: 'leaf',
-            key: '1-0-0-2',
+            title: "leaf",
+            key: "1-0-0-2",
           },
           {
-            title: 'leaf',
-            key: '1-0-0-3',
+            title: "leaf",
+            key: "1-0-0-3",
           },
         ],
       },
     ],
   },
   {
-    title: 'Education',
-    key: '2-0',
+    title: "Education",
+    key: "2-0",
     children: [
       {
-        title: 'parent 1-0',
-        key: '2-0-0',
+        title: "parent 1-0",
+        key: "2-0-0",
         children: [
           {
-            title: 'leaf',
-            key: '2-0-0-1',
+            title: "leaf",
+            key: "2-0-0-1",
           },
           {
-            title: 'leaf',
-            key: '2-0-0-2',
+            title: "leaf",
+            key: "2-0-0-2",
           },
           {
-            title: 'leaf',
-            key: '2-0-0-3',
+            title: "leaf",
+            key: "2-0-0-3",
           },
         ],
       },
@@ -107,8 +93,8 @@ type FolderStructureProps = {
 };
 
 const FolderStructure = ({ folderTitle }: FolderStructureProps) => {
-  const onSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info);
+  const onSelect: TreeProps["onSelect"] = (selectedKeys, info) => {
+    console.log("selected", selectedKeys, info);
   };
 
   return (
@@ -119,7 +105,7 @@ const FolderStructure = ({ folderTitle }: FolderStructureProps) => {
           className="folder-structure"
           blockNode={true}
           switcherIcon={<DownOutlined />}
-          defaultExpandedKeys={['0-0-0']}
+          defaultExpandedKeys={["0-0-0"]}
           onSelect={onSelect}
           treeData={treeData}
         />
